f0eb20c41cfcfd551937736943e2bbcaa810b72b
chuluq <choirulchuluq10@gmail.com>
2022-07-29 19:24:19 +0700

    login & signup forms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // pages & components
 import Home from "./pages/home";
+import Login from "./pages/login";
+import SignUp from "./pages/signUp";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
           </Routes>
         </div>
       </BrowserRouter>
